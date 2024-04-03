f4d641b9d7beeb8223b067bca11b6c301b5568cc
盖伦 <wanghongzhan@cai-inc.com>
2024-04-03 09:38:01 +0800

    first commit

---
+++ src/main/java/com/whz/dubbo/telnet/SelectTelnetHandler.java
@@ -0,0 +1,44 @@
+package com.whz.dubbo.telnet;
+
+import org.apache.dubbo.common.extension.Activate;
+import org.apache.dubbo.common.utils.CollectionUtils;
+import org.apache.dubbo.common.utils.StringUtils;
+import org.apache.dubbo.remoting.Channel;
+import org.apache.dubbo.remoting.telnet.TelnetHandler;
+import org.apache.dubbo.remoting.telnet.support.Help;
+
+import java.lang.reflect.Method;
+import java.util.List;
+ 
+/**
+ * SelectTelnetHandler
+ */
+@Activate
+@Help(parameter = "[index]", summary = "Select the index of the method you want to invoke.",
+        detail = "Select the index of the method you want to invoke.")
+public class SelectTelnetHandler implements TelnetHandler {
+    public static final String SELECT_METHOD_KEY = "telnet.select.method";
+    public static final String SELECT_KEY = "telnet.select";
+ 
+    private InvokeTelnetHandler invokeTelnetHandler = new InvokeTelnetHandler();
+ 
+    @Override
+    @SuppressWarnings("unchecked")
+    public String telnet(Channel channel, String message) {
+        if (message == null || message.length() == 0) {
+            return "Please input the index of the method you want to invoke, eg: \r\n select 1";
+        }
+        List<Method> methodList = (List<Method>) channel.getAttribute(InvokeTelnetHandler.INVOKE_METHOD_LIST_KEY);
+        if (CollectionUtils.isEmpty(methodList)) {
+            return "Please use the invoke command first.";
+        }
+        if (!StringUtils.isNumber(message) || Integer.parseInt(message) < 1 || Integer.parseInt(message) > methodList.size()) {
+            return "Illegal index ,please input select 1~" + methodList.size();
+        }
+        Method method = methodList.get(Integer.parseInt(message) - 1);
+        channel.setAttribute(SELECT_METHOD_KEY, method);
+        channel.setAttribute(SELECT_KEY, Boolean.TRUE);
+        String invokeMessage = (String) channel.getAttribute(InvokeTelnetHandler.INVOKE_MESSAGE_KEY);
+        return invokeTelnetHandler.telnet(channel, invokeMessage);
+    }
+}
